cb23a1b683f43a342c0e064df1c41c75ec37344a
alexandrakuzmina <kuzminash@list.ru>
2021-05-27 19:04:50 +0300

    rewrote whole main

--- src/main/kotlin/BotApplication.kt
+++ src/main/kotlin/BotApplication.kt
@@ -1,34 +1,27 @@
 import me.ivmg.telegram.bot
 import me.ivmg.telegram.dispatch
-import me.ivmg.telegram.dispatcher.command
 import me.ivmg.telegram.dispatcher.text
 import kotlin.random.Random.Default.nextInt
 
 fun main(args: Array<String>) {
 
-    val answers: Array<String> = arrayOf("100%", "MaybeBaby", "Hmmm.. Ask me one more time", "Definitely", "Wow Yas!!", "Maybe next time?", "I think no")
+    val replies: Array<String> = arrayOf("100%", "MaybeBaby", "Hmmm.. Ask me one more time", "Definitely", "Wow Yas!!", "Maybe next time?", "I think no")
     bot {
-        //token = "1239026618:AAFQFSOdPJcIWEpwW-jj8zXndLqqiLQ3weQ"
+        token = "1625641304:AAHCfC6cVMjLgLwFbyXJbFGDK9HVzroJO-U"
         dispatch {
             text { bot, update ->
-                if (update.message!!.text == "/start") {
-                    bot.sendMessage(
-                        chatId = update.message!!.chat.id,
-                        text = "Ask me a question and I will predict your future"
-                    )
-                }
-                else if (update.message!!.text!!.endsWith("?")) {
-                    val position = nextInt(0, answers.size - 1)
-                    bot.sendMessage(
-                        chatId = update.message!!.chat.id,
-                        text = answers[position]
-                    )
-                }
-                else {
-                    bot.sendMessage(
-                        chatId = update.message!!.chat.id,
-                        text = "It's not a question, question should ends with a ?"
-                    )
+                if (update.message?.text == "/start") {
+                    update.message?.let {
+                        bot.sendMessage(it.chat.id, "Ask me a question and I will predict your future")
+                    }
+                } else if (update.message?.text?.last() == '?') {
+                    update.message?.let {
+                        bot.sendMessage(it.chat.id, replies.random())
+                    }
+                } else {
+                    update.message?.let {
+                        bot.sendMessage(it.chat.id, "It's not a question, question should ends with a ?")
+                    }
                 }
             }
         }
